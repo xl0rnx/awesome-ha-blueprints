61f0d1c2066af6e8e046928eefa6d34130e15229
l0rn <delacecrew@gmail.com>
2024-02-07 06:18:35 +0100

    first commit

---
+++ website/sidebars.js
@@ -0,0 +1,26 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Getting Started',
+      collapsed: false,
+      items: [
+        'introduction',
+        'importing-a-blueprint',
+        'controllers-hooks-ecosystem',
+        'contributing',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Blueprints',
+      collapsed: false,
+      items: [
+        'blueprints',
+        'blueprints/controllers',
+        'blueprints/hooks',
+        'blueprints/automation',
+      ],
+    },
+  ],
+}
---
+++ website/src/components/ControllerImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ControllerImage({ children }) {
+  return <div style={{ width: 200 }}>{children}</div>
+}
+
+export default ControllerImage
